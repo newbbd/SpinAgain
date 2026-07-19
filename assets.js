d24d31ae7e0a0b9f44abe0c4390d6f5ede5816fc
newbd <super.ark81@gmail.com>
2026-07-18 18:20:16 -0700

    Register doodle artwork

---
+++ assets.js
@@ -0,0 +1,8 @@
+window.DOODLE_ASSETS = Object.freeze({
+  blackjack: "assets/blackjack.svg",
+  chips: "assets/chips.svg",
+  coin: "assets/coin.svg",
+  horse: "assets/horse.svg",
+  roulette: "assets/roulette.svg",
+  slot: "assets/slot.svg"
+});
